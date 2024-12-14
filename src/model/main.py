37e36566c8bff8de16f6de9a8483d1e49efc74fd
T0peerakarn <t0p.eerakarn@gmail.com>
2024-12-14 22:33:02 +0700

    feat: add wordcloud

--- src/model/main.py
+++ src/model/main.py
@@ -18,7 +18,28 @@ class Model:
     self.tokenizer = AutoTokenizer.from_pretrained(self.root_path)
     self.model = AutoModelForSequenceClassification.from_pretrained(self.root_path)
     self.sentiment_pipeline = pipeline("text-classification", model=self.model, tokenizer=self.tokenizer)
- 
+  
+  def clean_text(self, text):
+
+    url_pattern = r"((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*"
+    mention_pattern = r"@\w+"
+    hashtag_pattern = r"#\w+"
+    punctuation_pattern = r"[.,\/#!$%\^&\*;:{}=\-_`~()]"
+    
+    patterns = [
+      url_pattern,
+      mention_pattern,
+      hashtag_pattern,
+      punctuation_pattern
+    ]
+
+    for pattern in patterns:
+      text = re.sub(pattern, "", text)
+    
+    text = text.lower()
+
+    return text
+
   def get_attentions(self, text):
     inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True)
     outputs = self.model(**inputs, output_attentions=True)
@@ -34,11 +55,13 @@ class Model:
       else:
         word_importance.append([token, importance])
  
-    word_importance = [ [token, float(importance)] for token, importance in zip(tokens, token_importance) if token not in ["[CLS]", "[SEP]"] ]
+    word_importance = [ [token, float(importance)] for token, importance in word_importance if token not in ["[CLS]", "[SEP]"] ]
  
     return word_importance
  
   def analyse(self, text):
+    text = self.clean_text(text)
+
     result = self.sentiment_pipeline(text)
     sentiment = self.map_labels[result[0]["label"]]
     score = result[0]["score"]
