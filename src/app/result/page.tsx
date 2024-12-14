67f74b4a5b857e7dfd88717bdfa80b0464b9064b
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2024-12-14 21:12:29 +0700

    feat: add model, python script, api endpoint for analysis (#1)
    * feat: add analysis endpoint
    
    * feat: add python script
    
    * feat: add endpoint and script
    
    * fix: change model's dir name
    
    * fix: endpoint and python script
    
    * feat: setup git lfs and add .gitattribute
    
    * feat: add model
    
    * feat: add tokenizer

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from 'react';
-import WordCloud from '@/components/wordcloud';
+import React from "react";
+import WordCloud from "@/components/wordcloud";
 
 interface Sentiment {
   word: string;
@@ -17,23 +17,23 @@ interface Result {
 }
 
 const result: Result = {
-  "text": "I love the new design but the performance is terrible.",
-  "sentiments": [
-    {"word": "love", "sentiment": "positive", "weight": 0.9},
-    {"word": "design", "sentiment": "positive", "weight": 0.7},
-    {"word": "performance", "sentiment": "negative", "weight": 0.8},
-    {"word": "terrible", "sentiment": "negative", "weight": 0.9}
+  text: "I love the new design but the performance is terrible.",
+  sentiments: [
+    { word: "love", sentiment: "positive", weight: 0.9 },
+    { word: "design", sentiment: "positive", weight: 0.7 },
+    { word: "performance", sentiment: "negative", weight: 0.8 },
+    { word: "terrible", sentiment: "negative", weight: 0.9 },
   ],
-  "overallSentiment": "negative",
-  "confidenceScore": 0.61
+  overallSentiment: "negative",
+  confidenceScore: 0.61,
 };
 
 const getColor = (sentiment: string, weight: number) => {
   const opacity = weight;
   switch (sentiment) {
-    case 'positive':
+    case "positive":
       return `rgba(56, 166, 23, ${opacity})`; // green
-    case 'negative':
+    case "negative":
       return `rgba(203, 51, 51, ${opacity})`; // red
     default:
       return `rgba(0, 0, 0, ${opacity})`; // black
@@ -42,58 +42,71 @@ const getColor = (sentiment: string, weight: number) => {
 
 const getOverallSentimentColor = (sentiment: string) => {
   switch (sentiment) {
-    case 'positive':
-      return '#38A617'; // green
-    case 'negative':
-      return '#CB3333'; // red
+    case "positive":
+      return "#38A617"; // green
+    case "negative":
+      return "#CB3333"; // red
     default:
-      return 'black';
+      return "black";
   }
 };
 
 const ResultPage: React.FC = () => {
-  const words = result.sentiments.map(sentiment => ({
+  const words = result.sentiments.map((sentiment) => ({
     text: sentiment.word,
     size: sentiment.weight,
     sentiment: sentiment.sentiment,
   }));
 
   return (
-    <div >
+    <div>
       <header className="bg-[#5E62CC] text-white p-5 text-center w-full flex justify-between items-center">
-      <button className=" text-white px-4 py-1 rounded hover:{bg-white}" onClick={() => window.location.href = '/'}>
+        <button
+          className=" text-white px-4 py-1 rounded hover:{bg-white}"
+          onClick={() => (window.location.href = "/")}
+        >
           Back to Home
         </button>
         <h1>NTN Sentiment Analyzer</h1>
         <div></div>
-
       </header>
       <main className="px-60 py-5">
-        <h2 className='mb-5'>Inputted text: </h2>
+        <h2 className="mb-5">Inputted text: </h2>
         <section className="mb-5">
-        <div className="border rounded-lg p-4">
+          <div className="border rounded-lg p-4">
             <p>
-              {result.text.split(' ').map((word, index) => {
-                const sentiment = result.sentiments.find(s => s.word === word.toLowerCase());
-                const color = sentiment ? getColor(sentiment.sentiment, sentiment.weight) : 'black';
+              {result.text.split(" ").map((word, index) => {
+                const sentiment = result.sentiments.find(
+                  (s) => s.word === word.toLowerCase()
+                );
+                const color = sentiment
+                  ? getColor(sentiment.sentiment, sentiment.weight)
+                  : "black";
                 return (
-                  <span key={index} style={{ color, fontWeight: sentiment ? 'bold' : 'normal' }}>
-                    {word}{' '}
+                  <span
+                    key={index}
+                    style={{ color, fontWeight: sentiment ? "bold" : "normal" }}
+                  >
+                    {word}{" "}
                   </span>
                 );
               })}
             </p>
           </div>
         </section>
-        <h2 className='mb-5'>Overall sentiment: </h2>
-        <div className='flex items-center border rounded-lg p-4'>
+        <h2 className="mb-5">Overall sentiment: </h2>
+        <div className="flex items-center border rounded-lg p-4">
           <h2>It is determined that the sentiment of the given text(s) is: </h2>
-          <h2 className='text-3xl font-semibold ml-10'style={{ color: getOverallSentimentColor(result.overallSentiment) }}> 
-            {result.overallSentiment} ({result.confidenceScore})</h2>
+          <h2
+            className="text-3xl font-semibold ml-10"
+            style={{ color: getOverallSentimentColor(result.overallSentiment) }}
+          >
+            {result.overallSentiment} ({result.confidenceScore})
+          </h2>
         </div>
-          <div className="mt-5 w-full h-[400px] flex items-center">
-            <WordCloud words={words} width={1000} height={350}/>
-          </div>
+        <div className="mt-5 w-full h-[400px] flex items-center">
+          <WordCloud words={words} width={1000} height={350} />
+        </div>
       </main>
     </div>
   );
